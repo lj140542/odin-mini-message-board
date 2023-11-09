37c6b4e7676429190a0375987f0e5789b381f29e
lj140542 <jonathan.lefebvre76600@gmail.com>
2023-11-09 14:59:53 +0100

    Implements the functionality of the messageboard

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Messageboard', messages: messages });
+});
+
+/* GET new message's form */
+router.get('/new', function(req, res, next) {
+  res.render('form');
+});
+
+/* POST new message's form */
+router.post('/new', function(req, res, next) {
+  if (req.body.messageUser && req.body.messageText) {
+    messages.push({text: req.body.messageText, user: req.body.messageUser, added: new Date()});
+    res.redirect('/');
+  } else {
+    next();
+  }
 });
 
 module.exports = router;
